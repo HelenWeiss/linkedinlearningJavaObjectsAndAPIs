48c071022f41f462d900575b2c0adb3c9f08289e
HelenWeiss <elena.kherson@gmail.com>
2026-07-27 17:37:20 +0300

    Java, package exceptions: - class TryCatch; - numbers.txt.

--- src/exceptions/TryCatch.java
+++ src/exceptions/TryCatch.java
@@ -1,13 +1,19 @@
 package exceptions;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class TryCatch {
 
     public static void main(String[] args) {
 
+        // 1. catch one exception
+/*
         File file = new File("resources/nonexistent.txt");
+
         try{
             file.createNewFile(); // Unhandled exception: java.io.IOException
         } catch (IOException e) {
@@ -16,5 +22,29 @@ public class TryCatch {
             // at exceptions.TryCatch.main(TryCatch.java:12)
             e.printStackTrace();
         }
+*/
+        // handle multiple exceptions:
+        // series of catch blocks and multiple catch block
+        File fileNumbers = new File("src/numbers.txt");
+        System.out.println(fileNumbers.getAbsolutePath());
+        try{
+            Scanner fileReader = new Scanner(fileNumbers); // Unhandled exception: java.io.FileNotFoundException
+            while(fileReader.hasNext()) {
+                System.out.println(fileReader.nextDouble());
+            }
+       }
+        // 2. series of catch blocks
+        // instead of series of catch blocks are represented multiple catch block in lines 42-44
+/*      catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (InputMismatchException e) {
+            e.printStackTrace();
+        }
+*/
+        // 3. multiple catch block
+        catch (FileNotFoundException | InputMismatchException e) {
+            e.printStackTrace();  // at exceptions.TryCatch.main(TryCatch.java:33)
+        }
+
     }
 }
